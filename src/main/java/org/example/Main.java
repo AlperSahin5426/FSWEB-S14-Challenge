9f19e02cdff3a2bea1977aba62b4674c67a897fc
Alper Sahin <alpersahin5426@gmail.com>
2026-04-26 13:56:04 +0300

    Burger Challenge projesi tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.models.DeluxeBurger;
+import org.example.models.Hamburger;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic", "Normal", 3.56, "Wrap");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.itemizeHamburger();
+
+        System.out.println("---");
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition1("Should not work", 50.0);
+        db.itemizeHamburger();
     }
 }
---
+++ src/main/java/org/example/models/HealthyBurger.java
@@ -0,0 +1,39 @@
+package org.example.models;
+
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger(String name, double price, String breadRollType) {
+        super(name, "Tofu", price, breadRollType);
+    }
+
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+
+        super.addHamburgerAddition1(name, price);
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+        super.addHamburgerAddition2(name, price);
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        double totalPrice = super.itemizeHamburger();
+
+        if (healthyExtra1Name != null) {
+            System.out.println("HealthyAddition1: " + healthyExtra1Name);
+        }
+        if (healthyExtra2Name != null) {
+            System.out.println("HealthyAddition2: " + healthyExtra2Name);
+        }
+        return totalPrice;
+    }
+}
